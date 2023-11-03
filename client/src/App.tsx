72f5e3c0922692a7dcd82ffee6488aaca9f6a90d
Gokul Bansal <bansalgokul134@gmail.com>
2023-11-03 07:55:44 +0530

    Import Error fixed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Experience from "./components/experience/Experience";
 import Footer from "./components/footer/Footer";
-import Header from "./components/header/header";
+import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
 import Skills from "./components/skills/Skills";
 import Work from "./components/work/Work";
